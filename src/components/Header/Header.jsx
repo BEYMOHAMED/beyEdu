a5fdbb5f7ec2598a40d57dd9e275f2a8a3159a64
beymohamed <linkthebrave86@gmail.com>
2023-12-20 21:43:08 +0100

    fix logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,11 +7,11 @@ function Header({ toggleTheme, darkMode }) {
     <header className="bg-headerBGLight dark:bg-headerBGDark p-4 w-full mb-8">
       <div className="mx-auto flex justify-between items-center" style={{ maxWidth: '756px' }}>
         <div>
-          <img src={process.env.PUBLIC_URL + `assets/images/logo.PNG`} alt="Logo" className="h-8" />
+          <img src={process.env.PUBLIC_URL + `/assets/images/logo.PNG`} alt="Logo" className="h-8" />
         </div>
         <div>
           <button onClick={toggleTheme} className="w-10 h-10 bg-buttonBgLight hover:bg-buttonBgHoverLight dark:bg-buttonBgDark dark:hover:bg-buttonBgHoverDark rounded-lg flex justify-center items-center transition ease-in-out duration-300">
-            <img src={darkMode ? "assets/icons/sun.svg" : "assets/icons/moon.svg"} alt="Theme Icon" className="h-6" />
+            <img src={darkMode ? "/assets/icons/sun.svg" : "/assets/icons/moon.svg"} alt="Theme Icon" className="h-6" />
           </button>
         </div>
       </div>
