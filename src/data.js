8a4e082c60df7cbbf75bed53f22d6ab060cf2f32
beymohamed <linkthebrave86@gmail.com>
2024-04-10 22:44:37 +0200

    new layout

--- src/data.js
+++ src/data.js
@@ -5,7 +5,8 @@ const educationData = [
       "Ce cours offre une introduction au HTML et aborde les notions de base. Vous y trouverez des explications claires sur les balises HTML, la structure des pages, et comment débuter avec la création de sites web.",
     imageUrl: "/assets/images/html.png",
     courseDocumentUrl: "/assets/documents/courses/Introduction-HTML.pptx",
-    exercisesDocumentUrl: "/assets/documents/exercises/Exercice-HTML.pptx",
+    exercisesDocumentUrl:
+      "/assets/documents/exercises/Copy-of-Exercice-HTML.pptx",
   },
   {
     title: "HTML Forms",
@@ -13,6 +14,8 @@ const educationData = [
       "Ce cours se penche sur les formulaires HTML, un élément clé pour l'interaction sur le web. Destiné à ceux ayant une connaissance de base en HTML, il aborde la création et la gestion de formulaires web. Vous apprendrez à élaborer des formulaires pour la saisie utilisateur, en utilisant des champs de texte, des boutons radio, des menus déroulants et des boutons d'envoi. Le cours met également en lumière l'importance de la gestion des données de formulaire pour améliorer l'expérience utilisateur sur les sites web.",
     imageUrl: "/assets/images/form.png",
     courseDocumentUrl: "/assets/documents/courses/form-HTML.pptx",
+    exercisesDocumentUrl:
+      "/assets/documents/exercises/Copy-of-Exercice-Form.pptx",
   },
   {
     title: "Introduction au CSS",
@@ -20,26 +23,27 @@ const educationData = [
       "Ce cours vous initie au CSS, un outil essentiel pour le design web. Vous découvrirez comment utiliser le CSS pour styliser et organiser le contenu des pages web. Le cours couvre les sélecteurs, la syntaxe, le modèle de boîte, et bien plus. Parfait pour ceux qui souhaitent apprendre à créer des sites web visuellement attrayants et fonctionnels.",
     imageUrl: "/assets/images/css.png",
     courseDocumentUrl: "/assets/documents/courses/Introduction-au-CSS.pptx",
-    exercisesDocumentUrl: "/assets/documents/exercises/Exercice-CSS.pptx",
-  },
-  {
-    title: "Flex Box",
-    description:
-      "Ce cours se concentre sur Flexbox, une technique incontournable pour la mise en page en CSS. Vous apprendrez à maîtriser Flexbox pour créer des interfaces web réactives et élégantes. Le cours explore comment Flexbox simplifie l'alignement, la distribution et la disposition des éléments dans un conteneur. Idéal pour ceux qui veulent approfondir leurs compétences en CSS et concevoir des sites web modernes et adaptatifs.",
-    imageUrl: "/assets/images/flex-box.png",
-    courseDocumentUrl: "/assets/documents/courses/Flexbox.pptx",
-    exercisesDocumentUrl: "/assets/documents/exercises/Exercice-Flex.pptx",
-  },
-  {
-    title: "Media Queries",
-    description:
-      "Découvrez les media queries en CSS, essentiels pour créer des sites web adaptatifs. Ce cours vous initiera à la modification de la mise en page selon la taille de l'écran, idéal pour une conception web flexible et réactive.",
-    imageUrl: "/assets/images/meda-queries.png",
-    courseDocumentUrl:
-      "/assets/documents/courses/Introduction-aux-Media-Queries.pptx",
     exercisesDocumentUrl:
-      "/assets/documents/exercises/Exercices-Media-Query.pptx",
+      "/assets/documents/exercises/Copy-of-Exercice-CSS.pptx",
   },
+  // {
+  //   title: "Flex Box",
+  //   description:
+  //     "Ce cours se concentre sur Flexbox, une technique incontournable pour la mise en page en CSS. Vous apprendrez à maîtriser Flexbox pour créer des interfaces web réactives et élégantes. Le cours explore comment Flexbox simplifie l'alignement, la distribution et la disposition des éléments dans un conteneur. Idéal pour ceux qui veulent approfondir leurs compétences en CSS et concevoir des sites web modernes et adaptatifs.",
+  //   imageUrl: "/assets/images/flex-box.png",
+  //   courseDocumentUrl: "/assets/documents/courses/Flexbox.pptx",
+  //   exercisesDocumentUrl: "/assets/documents/exercises/Exercice-Flex.pptx",
+  // },
+  // {
+  //   title: "Media Queries",
+  //   description:
+  //     "Découvrez les media queries en CSS, essentiels pour créer des sites web adaptatifs. Ce cours vous initiera à la modification de la mise en page selon la taille de l'écran, idéal pour une conception web flexible et réactive.",
+  //   imageUrl: "/assets/images/meda-queries.png",
+  //   courseDocumentUrl:
+  //     "/assets/documents/courses/Introduction-aux-Media-Queries.pptx",
+  //   exercisesDocumentUrl:
+  //     "/assets/documents/exercises/Exercices-Media-Query.pptx",
+  // },
   {
     title: "Cheat Sheet",
     description:
@@ -47,13 +51,13 @@ const educationData = [
     imageUrl: "/assets/images/cheat-sheet.png",
     courseDocumentUrl: "/assets/documents/courses/cheat-sheet.pdf",
   },
-  {
-    title: "Final Project",
-    description:
-      "Utiliser les compétences acquises en HTML et CSS pour créer un site web complet et responsive sur un thème de votre choix.",
-    imageUrl: "/assets/images/final-project.png",
-    courseDocumentUrl: "/assets/documents/courses/final-project.pdf",
-  },
+  // {
+  //   title: "Final Project",
+  //   description:
+  //     "Utiliser les compétences acquises en HTML et CSS pour créer un site web complet et responsive sur un thème de votre choix.",
+  //   imageUrl: "/assets/images/final-project.png",
+  //   courseDocumentUrl: "/assets/documents/courses/final-project.pdf",
+  // },
 ];
 
 export default educationData;
